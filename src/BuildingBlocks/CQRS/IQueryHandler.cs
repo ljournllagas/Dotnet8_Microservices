968687bc3f1b40f322d114e47e63ef0c8fbfc1e1
Ljourn Llagas <llagas.ljournkevin@bdo.com.ph>
2024-08-21 22:10:32 +0800

    Added a building blocks solution to add an abstraction for the MediatR IRequestHandler, and Added an initial code for CreateProductHandler

---
+++ src/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface IQueryHandler<in TQUery, TResponse>
+    : IRequestHandler<TQUery, TResponse>
+    where TQUery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
+
--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,10 +1,35 @@
-﻿namespace Catalog.API.Products.CreateProduct;
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
 public record CreateProductResult(Guid Id);
 
 
-internal class CreateProductCommandHandler
+internal class CreateProductCommandHandler 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create Product enmtity from command object
+       
 
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //TODO
+        //save to database
+        //return CreateProductResult result
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
 }
 
