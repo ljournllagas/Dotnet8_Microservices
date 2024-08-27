fceb3b0e57172eadd498067cfd60665793a97f0c
Ljourn Llagas <llagas.ljournkevin@bdo.com.ph>
2024-08-27 12:31:22 +0800

    Completed the Section 8

---
+++ src/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart should not be empty");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler(IBasketRepository _repository) 
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        await _repository.StoreBasket(command.Cart, cancellationToken);
+
+        return new StoreBasketResult(command.Cart.UserName);
+    }
+}
